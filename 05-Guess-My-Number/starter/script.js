2f5d6c9bf23ca9f923ccda58f24805ac315fd26b
MrAlarmy <blakerhorton@icloud.com>
2023-02-14 19:54:53 -0700

    My own Number Guesser game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,11 +10,54 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+let score = 20;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+  console.log(guess, typeof guess, secretNumber);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+    // When there is no input
+  } else if (guess === secretNumber) {
+    // When player wins
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.highscore').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '32rem';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      //When guess is too high
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+      score;
+    } else {
+      //When you get the score to 0
+      document.querySelector('.message').textContent = 'You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      // when the number is too low
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+      score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
 });
